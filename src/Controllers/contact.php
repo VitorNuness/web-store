02ec10dc87b0472accffbf07ee6d3b006982719b
VitorNuness <vitor1nuness@gmail.com>
2024-08-05 15:28:31 -0300

    Aula 36 - Refatorando a classe Connector

--- src/Controllers/contact.php
+++ src/Controllers/contact.php
@@ -7,18 +7,16 @@ use Core\Database\Connector;
 $title = 'Contact Us';
 $heading = $title;
 $messageWasSent = false;
+$failure = false;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     /** @var Connector $db */
     $db = container(Connector::class);
 
-    try {
-        $result = $db->query('INSERT INTO messages (name, email, source, message) VALUES (:name, :email, :source, :message)', $_POST)
-            ->insert();
+    $id = $db->query('INSERT INTO messages (name, email, source, message) VALUES (:name, :email, :source, :message)', $_POST)
+        ->insert();
 
-        $messageWasSent = true;
-    } catch (\Exception $e) {
-    }
+    !$id ? $failure = true : $messageWasSent = true;
 }
 
 require resource_path('/views/contact.php');
